dda30bb81c425e90b9f15f582b9f75156d7eff13
MarcinGladkowski <marcingladkowski@gmail.com>
2025-04-25 16:00:49 +0200

    feat(shortener): add configure options to the algorithm

--- src/Service/Rot13HashedShortenedAlgorithm.php
+++ src/Service/Rot13HashedShortenedAlgorithm.php
@@ -4,10 +4,16 @@ declare(strict_types=1);
 
 namespace App\Service;
 
-final class Rot13HashedShortenedAlgorithm implements ShortenerAlgorithmInterface
+final readonly class Rot13HashedShortenedAlgorithm implements ShortenerAlgorithmInterface
 {
+    public function __construct(
+        private int $length = 8,
+        private string $hashFunction = 'sha256',
+    ) {
+    }
+
     public function hash(string $value): string
     {
-        return substr(hash('sha256', str_rot13($value)), 0, 8);
+        return substr(hash($this->hashFunction, str_rot13($value)), 0, $this->length);
     }
 }
