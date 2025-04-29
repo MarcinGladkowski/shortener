22c03ef679d7ebfd5b0ee3913cab5bd588b334c0
MarcinGladkowski <marcingladkowski@gmail.com>
2025-04-29 15:36:04 +0200

    feat(shortener): using service in controller

--- src/Controller/ShortenerController.php
+++ src/Controller/ShortenerController.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Service\Rot13HashedShortenedAlgorithm;
+use App\Service\Shortener;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -13,8 +16,16 @@ use Symfony\Component\Routing\Attribute\Route;
 final class ShortenerController
 {
     #[Route(path: '/shorten', methods: [Request::METHOD_POST])]
-    public function shorten(): JsonResponse
+    public function shorten(Request $request): JsonResponse
     {
-        return new JsonResponse(['value' => 'test']);
+        $value = $request->getPayload()->get('value');
+
+        if (null === $value) {
+            return new JsonResponse(['error' => 'Value is required'], Response::HTTP_BAD_REQUEST);
+        }
+
+        $shortenedValue = new Shortener(new Rot13HashedShortenedAlgorithm())->generate($value);
+
+        return new JsonResponse(['value' => $shortenedValue]);
     }
 }
--- tests/Controller/ShortenerControllerTest.php
+++ tests/Controller/ShortenerControllerTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 class ShortenerControllerTest extends WebTestCase
 {
@@ -15,7 +16,27 @@ class ShortenerControllerTest extends WebTestCase
         $client
             ->request('POST', '/shorten');
 
+        self::assertResponseStatusCodeSame(Response::HTTP_BAD_REQUEST);
+    }
+
+    /**
+     * @throws \JsonException
+     */
+    public function testReturnValueInResponse(): void
+    {
+        $client = static::createClient();
+
+        $client
+            ->request(
+                method: 'POST',
+                uri: '/shorten',
+                content: json_encode(['value' => 'test'], JSON_THROW_ON_ERROR)
+            );
+
+        $jsonResponse = json_decode($client->getResponse()->getContent(), true, 512, JSON_THROW_ON_ERROR);
+
         self::assertResponseIsSuccessful();
+        self::assertArrayHasKey('value', $jsonResponse);
     }
 
     /**
@@ -35,7 +56,6 @@ class ShortenerControllerTest extends WebTestCase
         $jsonResponse = json_decode($client->getResponse()->getContent(), true, 512, JSON_THROW_ON_ERROR);
 
         self::assertResponseIsSuccessful();
-        self::assertArrayHasKey('value', $jsonResponse);
+        self::assertEquals('f25f2772', $jsonResponse['value']);
     }
-
 }
